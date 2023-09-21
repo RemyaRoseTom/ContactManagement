fafbca5fd1b20ff3767efb30a636162d2ef4b0c7
RemyaRoseTom <remyarosetom@gmail.com>
2023-09-21 13:30:23 -0400

    Add files via upload

---
+++ ContactManager/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ContactManager
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Contact", action = "Index", id = "" }  // Parameter defaults
+            );
+        }
+    }
+}
