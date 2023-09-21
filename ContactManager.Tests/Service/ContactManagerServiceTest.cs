ee9f06e90e7d720ec9820abdfe10f25fefb52626
RemyaRoseTom <remyarosetom@gmail.com>
2023-09-21 14:38:17 -0400

    Add files via upload

---
+++ ContactManager.Tests/Service/ContactManagerServiceTest.cs
@@ -0,0 +1,94 @@
+﻿using ContactManager.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Mvc;
+
+namespace ContactManager.Tests.Service
+{
+    [TestClass]
+    public class ContactManagerServiceTest
+    {
+        private Mock<IContactManagerRepository> _mockRepository;
+        private ModelStateDictionary _modelState;
+        private IContactManagerService _service;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IContactManagerRepository>();
+            _modelState = new ModelStateDictionary();
+            _service = new ContactManagerService(new ModelStateWrapper(_modelState), _mockRepository.Object);
+        }
+
+        [TestMethod]
+        public void CreateContact()
+        {
+            var contact = new Contact { FirstName = "Peter", LastName = "Abraham", Phone = "555-5555", Email = "steve@somewhere.com" };
+
+            // Act
+            var result = _service.CreateContact(contact);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CreateContactRequiredFirstName()
+        {
+            // Arrange
+            var contact = new Contact { FirstName = string.Empty, LastName = "Abraham", Phone = "555-5555", Email = "steve@somewhere.com" };
+
+            // Act
+            var result = _service.CreateContact(contact);
+
+            // Assert
+            Assert.IsFalse(result);
+            var error = _modelState["FirstName"].Errors[0];
+            Assert.AreEqual("First name is required.", error.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void CreateContactRequiredLastName()
+        {
+            // Arrange
+            var contact = new Contact { FirstName = "Peter", LastName = string.Empty , Phone = "555-5555", Email = "steve@somewhere.com" };
+
+            // Act
+            var result = _service.CreateContact(contact);
+
+            // Assert
+            Assert.IsFalse(result);
+            var error = _modelState["LastName"].Errors[0];
+            Assert.AreEqual("Last name is required.", error.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void CreateContactInvalidPhone()
+        {
+            // Arrange
+            var contact = new Contact { FirstName = "Peter", LastName = "Abraham", Phone = "apple", Email = "steve@somewhere.com" };
+
+            // Act
+            var result = _service.CreateContact(contact);
+
+            // Assert
+            Assert.IsFalse(result);
+            var error = _modelState["Phone"].Errors[0];
+            Assert.AreEqual("Invalid phone number.", error.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void CreateContactInvalidEmail()
+        {
+            // Arrange
+            var contact = new Contact { FirstName = "Peter", LastName = "Abraham", Phone = "555-555", Email = "apple" };
+
+            // Act
+            var result = _service.CreateContact(contact);
+
+            // Assert
+            Assert.IsFalse(result);
+            var error = _modelState["Email"].Errors[0];
+            Assert.AreEqual("Invalid email address.", error.ErrorMessage);
+        }
+    }
+}
